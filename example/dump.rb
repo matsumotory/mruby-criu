7baf997f67e7eaef55356cb5f7a7985dca6d3cd9
MATSUMOTO Ryosuke <matsumoto_r@net.ist.i.kyoto-u.ac.jp>
2014-04-25 22:34:38 +0900

    Update example

--- example/dump.rb
+++ example/dump.rb
@@ -1,13 +1,15 @@
 socket = "/var/run/criu_service.socket"
-images = "/tmp/dump_test"
+images = "/var/cache/criu_dump"
 log = "dump.log"
-pid = 4823
+# need iij/mruby-env
+pid = ENV["PID"].to_i
 
 c = CRIU.new
 p c.set_pid pid
 p c.set_images_dir images
 p c.set_service_address socket
-#p c.set_shell_job true
+#p c.set_ext_unix_sk true
+#p c.set_tcp_established true
 p c.set_log_file log
 
 p c.dump
--- example/restore.rb
+++ example/restore.rb
@@ -1,13 +1,12 @@
 socket = "/var/run/criu_service.socket"
-images = "/tmp/dump_test"
+images = "/var/cache/criu_dump"
 log = "restore.log"
-#pid = 3478
 
 c = CRIU.new
 p c.set_service_address socket
-#p c.set_pid pid
 p c.set_images_dir images
-#p c.set_shell_job true
 p c.set_log_file log
+#p c.set_ext_unix_sk true
+#p c.set_tcp_established true
 
 p c.restore
